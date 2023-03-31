61132aa404e3d1630649bd16ad299bf6cb655b3e
gustavoprorim <gustavo.silva@prorim.org.br>
2023-03-31 15:29:32 -0300

    att

--- mailing.py
+++ mailing.py
@@ -18,7 +18,14 @@ df['NR_CPF'] = df['NR_CPF'].astype(str)
 df['NR_CPF'] = df['NR_CPF'].apply(lambda x: x.zfill(11))
 df['NR_CPF'] = df['NR_CPF'].apply(lambda x: re.sub(r'(\d{3})(\d{3})(\d{3})(\d{2})', r'\1.\2.\3-\4', x))
 
-df['FONE1'] = df['FONE1'].apply(lambda x: re.sub(r'^\d{2}\s', '', str(x)))
-df['FONE2'] = df['FONE2'].apply(lambda x: re.sub(r'^\d{2}\s', '', str(x)))
+df['DDD'] = df[['DDD1', 'DDD2']].apply(lambda x: x.dropna().astype(str).str.replace('-', '').str.zfill(2).str.extract(r'(\d{2})', expand=False).iloc[0] if len(x.dropna().astype(str).str.replace('-', '').str.zfill(2).str.extract(r'(\d{2})', expand=False)) > 0 else None, axis=1)
+
+df['FONE'] = df[['FONE1', 'FONE2']].apply(lambda x: x.dropna().astype(str).str.replace(r'^\d{2}\s', '').str.extract(r'(\d{8,9})', expand=False).iloc[0] if len(x.dropna().astype(str).str.replace(r'^\d{2}\s', '').str.extract(r'(\d{8,9})', expand=False)) > 0 else None, axis=1)
+
+df = df.drop(['DDD1', 'DDD2', 'FONE1', 'FONE2'], axis=1)
+
+df = df.drop_duplicates(subset=['NOME', 'NR_CPF'])
+
+df = df.dropna(subset=['DDD', 'FONE'])
 
 df.to_excel('padraomailing.xlsx', index=False)
